fee79c80500f0f9a2588d8f194fa90009ba72846
Noah Orensa <noahorensa@gmail.com>
2020-07-19 09:51:28 -0600

    initial implementations of adding/editing/searching for business & events

--- backend/api/resolvers.js
+++ backend/api/resolvers.js
@@ -41,7 +41,7 @@ const businessUserLoader = new DataLoader(
   async (ids) => {
     return ids.map((id) => {
       return db.query(
-        "SELECT * FROM business_user WHERE id=?",
+        "SELECT * FROM business_user WHERE id = ?",
         [ id ]
       ).then(rows => rows[0]);
     });
@@ -52,16 +52,292 @@ const publicUserLoader = new DataLoader(
   async (ids) => {
     return ids.map((id) => {
       return db.query(
-        "SELECT * FROM public_user WHERE id=?",
+        "SELECT * FROM public_user WHERE id = ?",
         [ id ]
       ).then(rows => rows[0]);
     });
   }
 );
 
+const businessLoader = new DataLoader(
+  async (ids) => {
+    return ids.map((id) => {
+      return db.query(
+        `
+          SELECT
+            JSON_INSERT(
+              props,
+              '$.id', id,
+              '$.owner', owner,
+              '$.approved', approved,
+              '$.rating', calculated_rating,
+              '$.display_name', display_name,
+              '$.display_picture', display_picture,
+              '$.type', \`type\`,
+              '$.sub_type', sub_type
+            ) AS data
+          FROM
+            business
+          WHERE
+            id = ?
+        `,
+        [ id ]
+      ).then(rows => JSON.parse(rows[0].data));
+    });
+  }
+);
+
+const eventLoader = new DataLoader(
+  async (ids) => {
+    return ids.map((id) => {
+      return db.query(
+        `
+          SELECT
+          JSON_INSERT(
+            props,
+            '$.id', id,
+            '$.owner', owner,
+            '$.display_name', display_name,
+            '$.display_picture', display_picture,
+            '$.type', \`type\`,
+            '$.city', city,
+            '$.start', start,
+            '$.end', end,
+          ) AS data
+        FROM
+            event
+          WHERE
+            id = ?
+        `,
+        [ id ]
+      ).then(rows => JSON.parse(rows[0].data));
+    });
+  }
+);
+
+function _validateAuthenticatedBusinessUser(user) {
+  if (! user || user.type != 'business') {
+    throw new ApolloError("No or invalid authentication", 'USER_NOT_AUTHENTICATED');
+  }
+}
+
+function _validateAuthenticatedPublicUser(user) {
+  if (! user || user.type != 'public') {
+    throw new ApolloError("No or invalid authentication", 'USER_NOT_AUTHENTICATED');
+  }
+}
+
+function _validateBusinessOwner(user, businessId) {
+  var business = await businessLoader.load(businessId);
+  if (business.owner != user.id) {
+    throw new ApolloError(
+      "You're not the owner of this business! This incident will be reported.", 
+      'NOT_BUSINESS_OWNER'
+    );
+  }
+}
+
+function _validateBusinessOwnerAndType(user, businessId, type) {
+  var business = await businessLoader.load(businessId);
+  if (business.owner != user.id) {
+    throw new ApolloError(
+      "You're not the owner of this business! This incident will be reported.", 
+      'NOT_BUSINESS_OWNER'
+    );
+  }
+  if (business.type != type) {
+    throw new ApolloError(
+      "Invalid business type.", 
+      'INVALID_BUSINESS_TYPE'
+    );
+  }
+}
+
+function _validateEventOwner(user, eventId) {
+  var event = await eventLoader.load(eventId);
+  if (event.owner != user.id) {
+    throw new ApolloError(
+      "You're not the owner of this event! This incident will be reported.", 
+      'NOT_EVENT_OWNER'
+    );
+  }
+}
+
+async function _addBusiness(data, owner) {
+  try {
+    var props = data;
+
+    var display_name = data.display_name; delete props.display_name;
+    var display_picture = data.display_picture; delete props.display_picture;
+    var type = data.type; delete props.type;
+    var sub_type = data.sub_type; delete props.sub_type;
+
+    const result = await db.query(
+      `
+        INSERT INTO business
+          (display_name, display_picture, type, sub_type, props, owner)
+        VALUES
+          (?, ?, ?, ?, ?, ?)
+      `,
+      [
+        display_name,
+        display_picture,
+        type,
+        sub_type,
+        JSON.stringify(props),
+        owner
+      ]
+    );
+    id = result.insertId;
+
+    // TODO: send confirmation email
+
+    return id;
+  }
+  catch(e) {
+    console.log(e);
+    throw new ApolloError("Failed to add businesss.", 'ADD_BUSINESS_FAILED');
+  }
+}
+
+async function _updateBusiness(id, data) {
+  try {
+    var stringifiedData = JSON.stringify(data);
+    await db.query(
+      `
+        INSERT INTO business_tentative_update
+          (business_id, updated_data)
+        VALUES
+          (?, ?)
+        ON DUPLICATE KEY UPDATE
+          updated_data = JSON_MERGE_PATCH(updated_data, ?)
+      `,
+      [
+        id,
+        stringifiedData,
+        stringifiedData
+      ]
+    );
+
+    // TODO: send confirmation email
+  }
+  catch(e) {
+    console.log(e);
+    throw new ApolloError("Failed to update businesss.", 'UPDATE_BUSINESS_FAILED');
+  }
+}
+
+async function _addEvent(data, owner) {
+  try {
+    var props = data;
+
+    var display_name = data.display_name; delete props.display_name;
+    var display_picture = data.display_picture; delete props.display_picture;
+    var type = data.type; delete props.type;
+    var city = data.city; delete props.city;
+    var start = data.start; delete props.start;
+    var end = data.end; delete props.end;
+
+    const result = await db.query(
+      `
+        INSERT INTO event
+          (
+            owner,
+            display_name,
+            display_picture,
+            type,
+            city,
+            start,
+            end,
+            props
+          )
+        VALUES
+          (?, ?, ?, ?, ?, ?, ?, ?)
+      `,
+      [
+        owner,
+        display_name,
+        display_picture,
+        type,
+        city,
+        start,
+        end,
+        JSON.stringify(props),
+      ]
+    );
+    id = result.insertId;
+
+    // TODO: send confirmation email
+
+    return id;
+  }
+  catch(e) {
+    console.log(e);
+    throw new ApolloError("Failed to add event.", 'ADD_EVENT_FAILED');
+  }
+}
+
+async function _updateEvent(id, data) {
+  try {
+    var fields = [];
+    var args = []
+
+    if (data.display_name) {
+      fields.push("display_name = ?");
+      args.push(data.display_name);
+      delete data.display_name;
+    }
+
+    if (data.display_picture) {
+      fields.push("display_picture = ?");
+      args.push(data.display_picture);
+      delete data.display_picture;
+    }
+
+    if (data.type) {
+      fields.push("type = ?");
+      args.push(data.type);
+      delete data.type;
+    }
+
+    if (data.city) {
+      fields.push("city = ?");
+      args.push(data.city);
+      delete data.city;
+    }
+
+    if (data.start) {
+      fields.push("start = ?");
+      args.push(data.start);
+      delete data.start;
+    }
+
+    if (data.end) {
+      fields.push("end = ?");
+      args.push(data.end);
+      delete data.end;
+    }
+
+    if (Object.keys(data).length > 0) {
+      fields.push("props = JSON_MERGE_PATCH(props, ?)");
+      args.push(JSON.stringify(data));
+    }
+
+    args.push(id);
+
+    await db.query("UPDATE event SET " + fields.join(', ') + " WHERE id = ?;", args);
+
+    // TODO: send confirmation email
+  }
+  catch(e) {
+    console.log(e);
+    throw new ApolloError("Failed to update event.", 'UPDATE_EVENT_FAILED');
+  }
+}
+
 const resolvers = {
   Query: {
-    async user(root, args, { user }) {
+    async user(_, args, { user }) {
       if (! user) {
         throw new ApolloError("Sorry... You're not authenticated! :c", 'USER_NOT_AUTHENTICATED');
       }
@@ -82,6 +358,91 @@ const resolvers = {
           'INVALID_USER_TYPE'
         );
       }
+    },
+
+    async businesses(_, { limit, offset, type, sub_type }, { user }) {
+      _validateAuthenticatedPublicUser();
+
+      var result;
+      if (type && sub_type) {
+        result = await db.query(
+          `
+            SELECT
+              JSON_INSERT(
+                props,
+                '$.id', id,
+                '$.approved', approved,
+                '$.rating', calculated_rating,
+                '$.display_name', display_name,
+                '$.display_picture', display_picture,
+                '$.type', \`type\`,
+                '$.sub_type', sub_type
+              ) AS data
+            FROM
+              business
+            WHERE
+              type = ? AND
+              sub_type = ?
+            LIMIT ?
+            OFFSET ?
+          `,
+          [type, sub_type, limit, offset]
+        );
+      }
+      else if (type) {
+        result = await db.query(
+          `
+            SELECT
+              JSON_INSERT(
+                props,
+                '$.id', id,
+                '$.approved', approved,
+                '$.rating', calculated_rating,
+                '$.display_name', display_name,
+                '$.display_picture', display_picture,
+                '$.type', \`type\`,
+                '$.sub_type', sub_type
+              ) AS data
+            FROM
+              business
+            WHERE
+              type = ?
+            LIMIT ?
+            OFFSET ?
+          `,
+          [type, limit, offset]
+        );
+      }
+      else {
+        result = await db.query(
+          `
+            SELECT
+              JSON_INSERT(
+                props,
+                '$.id', id,
+                '$.approved', approved,
+                '$.rating', calculated_rating,
+                '$.display_name', display_name,
+                '$.display_picture', display_picture,
+                '$.type', \`type\`,
+                '$.sub_type', sub_type
+              ) AS data
+            FROM
+              business
+            LIMIT ?
+            OFFSET ?
+          `,
+          [limit, offset]
+        );
+      }
+
+      return result.map(row => JSON.parse(row.data))
+    },
+
+    async events(_, { limit, offset, type }, { user }) {
+      _validateAuthenticatedPublicUser(user);
+
+      return [];
     }
   },
 
@@ -96,7 +457,8 @@ const resolvers = {
         );
       }
       catch(e) {
-        throw new ApolloError("Failed to signup.", 'PUBLIC_USER_SIGNUP_REJECTED');
+        console.log(e);
+        throw new ApolloError("Failed to signup.", 'PUBLIC_USER_SIGNUP_FAILED');
       }
 
       // return json web token
@@ -119,7 +481,8 @@ const resolvers = {
         // TODO: send confirmation email
       }
       catch(e) {
-        throw new ApolloError("Failed to signup.", 'BUSINESS_USER_SIGNUP_REJECTED');
+        console.log(e);
+        throw new ApolloError("Failed to signup.", 'BUSINESS_USER_SIGNUP_FAILED');
       }
 
       // return json web token
@@ -150,7 +513,7 @@ const resolvers = {
         }
         else {
           throw new ApolloError(
-            "Invalid ID. Please make sure the ID is correct or signup again.",
+            "Invalid ID. Please make sure the user ID is correct or signup again to obtain a new ID",
             'PUBLIC_USER_LOGIN_REJECTED'
           );
         }
@@ -158,7 +521,7 @@ const resolvers = {
       catch(e) {
         throw new ApolloError(
           "Failed to login.",
-          'PUBLIC_USER_LOGIN_FAILURE'
+          'PUBLIC_USER_LOGIN_FAILED'
         );
       }
     },
@@ -175,11 +538,9 @@ const resolvers = {
         console.log(e);
         throw new ApolloError(
           "Failed to login.",
-          'BUSINESS_USER_LOGIN_FAILURE'
+          'BUSINESS_USER_LOGIN_FAILED'
         );
       }
-
-      console.log(await bcrypt.hash(password, 10));
 
       if (result.length == 1 && await bcrypt.compare(password, result[0].password)) {
         const id = result[0].id;
@@ -208,8 +569,271 @@ const resolvers = {
           'BUSINESS_USER_LOGIN_REJECTED'
         );
       }
+    },
+
+    async changeBusinessUserPassword(_, { oldPassword, newPassword }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+
+      // TODO: implement
+    },
+
+    async addSelfEmployedBusiness(_, { data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+
+      data.type = 'SelfEmployedBusiness';
+
+      var id = await _addBusiness(data, user.id);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async updateSelfEmployedBusiness(_, { id, data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+      _validateBusinessOwnerAndType(user, id, 'SelfEmployedBusiness');
+
+      id = await _updateBusiness(id, data);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async addChildEducationBusiness(_, { data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+
+      data.type = 'ChildEducationBusiness';
+
+      var id = await _addBusiness(data, user.id);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async updateChildEducationBusiness(_, { id, data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+      _validateBusinessOwnerAndType(user, id, 'ChildEducationBusiness');
+
+      id = await _updateBusiness(id, data);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async addDomesticHelpBusiness(_, { data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+
+      data.type = 'DomesticHelpBusiness';
+
+      var id = await _addBusiness(data, user.id);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async updateDomesticHelpBusiness(_, { id, data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+      _validateBusinessOwnerAndType(user, id, 'DomesticHelpBusiness');
+
+      id = await _updateBusiness(id, data);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async addBeautyBusiness(_, { data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+
+      data.type = 'BeautyBusiness';
+
+      var id = await _addBusiness(data, user.id);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async updateBeautyBusiness(_, { id, data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+      _validateBusinessOwnerAndType(user, id, 'BeautyBusiness');
+
+      id = await _updateBusiness(id, data);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async addLimousineBusiness(_, { data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+
+      data.type = 'LimousineBusiness';
+
+      var id = await _addBusiness(data, user.id);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async updateLimousineBusiness(_, { id, data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+      _validateBusinessOwnerAndType(user, id, 'LimousineBusiness');
+
+      id = await _updateBusiness(id, data);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async addHospitalityBusiness(_, { data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+
+      data.type = 'HospitalityBusiness';
+
+      var id = await _addBusiness(data, user.id);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async updateHospitalityBusiness(_, { id, data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+      _validateBusinessOwnerAndType(user, id, 'HospitalityBusiness');
+
+      id = await _updateBusiness(id, data);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async addStationaryBusiness(_, { data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+
+      data.type = 'StationaryBusiness';
+
+      var id = await _addBusiness(data, user.id);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async updateStationaryBusiness(_, { id, data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+      _validateBusinessOwnerAndType(user, id, 'StationaryBusiness');
+
+      id = await _updateBusiness(id, data);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async addMadeInQatarBusiness(_, { data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+
+      data.type = 'MadeInQatarBusiness';
+
+      var id = await _addBusiness(data, user.id);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async updateMadeInQatarBusiness(_, { id, data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+      _validateBusinessOwnerAndType(user, id, 'MadeInQatarBusiness');
+
+      id = await _updateBusiness(id, data);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async addSportsBusiness(_, { data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+
+      data.type = 'SportsBusiness';
+
+      var id = await _addBusiness(data, user.id);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async updateSportsBusiness(_, { id, data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+      _validateBusinessOwnerAndType(user, id, 'SportsBusiness');
+
+      id = await _updateBusiness(id, data);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async addEntertainmentBusiness(_, { data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+
+      data.type = 'EntertainmentBusiness';
+
+      var id = await _addBusiness(data, user.id);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async updateEntertainmentBusiness(_, { id, data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+      _validateBusinessOwnerAndType(user, id, 'EntertainmentBusiness');
+
+      id = await _updateBusiness(id, data);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async addFoodBusiness(_, { data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+
+      data.type = 'FoodBusiness';
+
+      var id = await _addBusiness(data, user.id);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async updateFoodBusiness(_, { id, data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+      _validateBusinessOwnerAndType(user, id, 'FoodBusiness');
+
+      id = await _updateBusiness(id, data);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async addMaintenanceBusiness(_, { data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+
+      data.type = 'MaintenanceBusiness';
+
+      var id = await _addBusiness(data, user.id);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async updateMaintenanceBusiness(_, { id, data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+      _validateBusinessOwnerAndType(user, id, 'MaintenanceBusiness');
+
+      id = await _updateBusiness(id, data);
+      var business = await businessLoader.load(id);
+      return business;
+    },
+
+    async addEvent(_, { data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+
+      var id = await _addEvent(data, user.id);
+      var event = await eventLoader.load(id);
+      return event;
+    },
+
+    async updateEvent(_, { id, data }, { user }) {
+      _validateAuthenticatedBusinessUser(user);
+      _validateEventOwner(user, id);
+
+      id = await _updateEvent(id, data);
+      var event = await eventLoader.clear(id).load(id);
+      return event;
     }
-  }
+  },
+
+  Business: {
+    __resolveType(obj, ctx, info) {
+      return obj.type;
+    }
+  },
+
+  DomesticHelpPersonnel: {
+    __resolveType(obj, ctx, info) {
+      return obj.profession;
+    }
+  },
 }
 
 module.exports = resolvers;

2b79634b9fd298b4a5ee0b521cb865596427cfbb
Noah Orensa <noahorensa@gmail.com>
2020-09-12 17:38:36 -0600

    adjust & revise business forms

--- webui/domestic_help_form.js
+++ webui/domestic_help_form.js
@@ -7,4 +7,24 @@ $(document).ready(function () {
   GraphQL.fillOptionsFromEnum("City", [
     "domestic-help-city"
   ]);
+
+  GraphQL.fillOptionsFromEnum("Profession", [
+    "domestic-help-personnel-profession"
+  ]);
+
+  GraphQL.fillOptionsFromEnum("Gender", [
+    "domestic-help-personnel-gender"
+  ]);
+
+  GraphQL.fillOptionsFromEnum("MaritalStatus", [
+    "domestic-help-personnel-marital-status"
+  ]);
+
+  GraphQL.fillOptionsFromEnum("Education", [
+    "domestic-help-personnel-education"
+  ]);
+
+  GraphQL.fillOptionsFromEnum("Currency", [
+    "domestic-help-personnel-salary-currency"
+  ]);
 });

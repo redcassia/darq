7ac52509656f6e51bb5422b1b8fb715016488a02
Noah Orensa <noahorensa@gmail.com>
2020-09-20 18:20:05 -0600

    use ssl certificate for webui server

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,25 @@
 require('dotenv').config()
 var api_app = require('./api')
 var webui_app = require('./webui')
+const https = require('https')
+const fs = require('fs')
 
 api_app.listen(
     process.env.API_PORT, 
     () => console.log('GraphQL API server started on localhost:' + process.env.API_PORT + '/api')
 );
 
-webui_app.listen(
-    process.env.WEBUI_PORT,
-    () => console.log('WebUI server started on localhost:' + process.env.WEBUI_PORT)
-);
+if (process.env.CERT_FILE > 0) {
+    https.createServer({
+        key: fs.readFileSync(process.env.KEY_FILE),
+        cert: fs.readFileSync(process.env.CERT_FILE)
+    }).listen(process.env.WEBUI_PORT);
+    console.log('WebUI server started on localhost:' + process.env.WEBUI_PORT);
+}
+else {
+    console.log('WARNING: no SSL certificate found!')
+    webui_app.listen(
+        process.env.WEBUI_PORT,
+        () => console.log('WebUI server started on localhost:' + process.env.WEBUI_PORT)
+    );
+}

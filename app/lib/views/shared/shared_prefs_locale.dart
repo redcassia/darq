24e9446d16b856f614277546c4b482ed158f49c9
Salvia Elmassry <salvya.elmassry.se@gmail.com>
2020-10-19 16:38:51 +0200

    edit ios appIcon

---
+++ app/lib/views/shared/shared_prefs_locale.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future setLocale(String locale, BuildContext context) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString('locale', locale);
+}

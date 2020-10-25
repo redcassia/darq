31804de923b5ad77ec11811f316a8db343697472
Salvia Elmassry <salvya.elmassry.se@gmail.com>
2020-10-25 03:31:55 +0200

    major changes

--- app/lib/views/home/screens/home.dart
+++ app/lib/views/home/screens/home.dart
@@ -1,11 +1,10 @@
 import 'dart:convert';
 import 'dart:io';
-
-import 'package:chewie/chewie.dart';
-import 'package:chewie/src/chewie_player.dart';
 import 'package:darq/elements/app_fonts.dart';
 import 'package:darq/presentation/custom_icons.dart';
+import 'package:darq/res/path_files.dart';
 import 'package:darq/utilities/constants.dart';
+import 'package:darq/utilities/screen_info.dart';
 import 'package:darq/views/shared/app_bars/default_appbar.dart';
 import 'package:darq/views/shared/button.dart';
 import 'package:darq/views/shared/default_card.dart';
@@ -18,7 +17,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_translate/flutter_translate.dart';
 import 'package:link/link.dart';
 import 'package:twitter_api/twitter_api.dart';
-import 'package:video_player/video_player.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class Home extends StatefulWidget {
@@ -28,41 +26,118 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  final String DARQ_WEBSITE = "darq.qa";
+
   int _currentIndex = 0;
-  int liveOrientation = 4;
-  String _screenTitle;
+  int position = 1;
+  bool circularProgressIndicator = true;
   bool _showBottomNavBar = true;
-  IconData orientation = Icons.fullscreen;
-  static ChewieController _chewieController;
-  static VideoPlayerController _controller;
-  List<Widget> _children;
+
+  dynamic _twitterAuth;
+
   String consumerApiKey = "dlBPaKPqJjtl7BoUfqs6GkdDM";
   String consumerApiSecret =
       "6hCeiXyQcu4jVNYVABLB1B3g25C8WmbDCbmKPtye4B81Ic3knc";
   String accessToken = "1303130664905179143-bLIig9lBe2ZNWO0bM5UrSriOkBcbKm";
   String accessTokenSecret = "YYIPpyfI9HsxSVm4fw2dLqjTNh8KqnXODAyEBGngYWkH5";
-  dynamic _twitterAuth;
-  List<dynamic> tweets;
+
   String start = "\"";
   String end = "\"";
+  String _screenTitle;
 
-  void initializeChewieController() {
-    _chewieController = ChewieController(
-      autoInitialize: true,
-      videoPlayerController: _controller,
-      aspectRatio: 3 / 2,
-      allowMuting: true,
-      fullScreenByDefault: true,
-    );
+  List<dynamic> _tweets;
+  List<Widget> _tabs;
+
+  double screenWidth;
+
+  @override
+  void initState() {
+    super.initState();
+    _screenTitle = translate("news");
+    getTwitterNews();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  showAlertDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierColor: Colors.black.withOpacity(0.8),
+        builder: (BuildContext context) {
+          return Center(
+              child: Container(
+                  margin: EdgeInsets.all(20.w),
+                  padding: EdgeInsets.symmetric(vertical: 33.h),
+                  decoration: BoxDecoration(
+                      color: Color(0xFFFFFFFF),
+                      borderRadius:
+                          Localizations.localeOf(context).languageCode == 'en'
+                              ? BorderRadius.only(
+                                  topLeft: Radius.circular(40),
+                                  bottomRight: Radius.circular(40),
+                                )
+                              : BorderRadius.only(
+                                  topRight: Radius.circular(40),
+                                  bottomLeft: Radius.circular(40))),
+                  child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 30.w),
+                      child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Image(
+                                image: AssetImage(
+                                    PathFiles.ImgPath + "add_ur_business.png")),
+                            SizedBox(height: 19.h),
+                            RichText(
+                                textAlign: TextAlign.center,
+                                text: TextSpan(
+                                    style: AppFonts.text7w500(
+                                        color: Color(0xFF4D4D4D)),
+                                    children: <TextSpan>[
+                                      TextSpan(text: translate("if_you_wish")),
+                                      TextSpan(
+                                          text: translate("darq"),
+                                          style: AppFonts.text7w500(
+                                              color: Color(0xFF476C7B))),
+                                      TextSpan(text: translate("please_visit")),
+                                      TextSpan(
+                                          text: " $DARQ_WEBSITE",
+                                          style: AppFonts.text7w500(
+                                              color: Color(0xFF476C7B))),
+                                      TextSpan(
+                                          text:
+                                              translate("using_your_computer"))
+                                    ]))
+                            // dialog bottom
+                          ]))));
+        });
   }
 
   Future getTwitterNews() async {
-    _twitterAuth = new twitterApi(
+    _twitterAuth = twitterApi(
         consumerKey: consumerApiKey,
         consumerSecret: consumerApiSecret,
         token: accessToken,
         tokenSecret: accessTokenSecret);
-    const List<String> sources = ["AJABreaking", "ajmubasher", "MOI_Qatar"];
+    const List<String> sources = [
+      "AJABreaking",
+      "Qatar_Edu",
+      "ajmubasher",
+      "MOI_Qatar",
+      "MOTC_QA",
+      "HukoomiQatar",
+      "QF",
+      "NTC_Qatar",
+      "AwqafM",
+      "MOCIQatar",
+      "qatarairwaysar",
+      "qatarpetroleum",
+      "ADLSAQa",
+      "ahalqatar"
+    ];
     Future.wait(sources.map((src) async {
       var result = await _twitterAuth.getTwitterRequest(
           // Http Method
@@ -72,7 +147,7 @@ class _HomeState extends State<Home> {
           // The options for the request
           options: {
             "screen_name": src,
-            "count": "20",
+            "count": "10",
             "tweet_mode": "extended", // Used to prevent truncating tweets
           });
       return json.decode(result.body);
@@ -88,69 +163,68 @@ class _HomeState extends State<Home> {
             -1;
       });
       setState(() {
-        tweets = t;
+        position = 0;
+        _tweets = t;
       });
     });
   }
 
-  @override
-  void initState() {
-    super.initState();
-
-    getTwitterNews();
-
-    _controller = VideoPlayerController.network(
-        'http://qtv-i.akamaihd.net/hls/live/213233/QTV/master360p.m3u8');
-    initializeChewieController();
+  startLoading(String url) {
+    setState(() => position = 1);
   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    _chewieController.dispose();
-    super.dispose();
+  doneLoading(String url) {
+    setState(() => position = 0);
   }
 
   @override
   Widget build(BuildContext context) {
-    _screenTitle = translate("news");
+    screenWidth = SI.screenWidth;
     ScreenUtil.init(context, width: 375, height: 667, allowFontScaling: true);
-    _children = [
-      Tweets(tweets: tweets, start: start, end: end),
-      WebView(
-        initialUrl: 'https://www.qe.com.qa/wp/mws/tabs/tab1',
-        javascriptMode: JavascriptMode.unrestricted,
-        navigationDelegate: (NavigationRequest request) {
-          if (request.url.startsWith('https://www.youtube.com/')) {
-            print('blocking navigation to $request}');
-            return NavigationDecision.prevent;
-          }
-          print('allowing navigation to $request');
-          return NavigationDecision.navigate;
-        },
-        onPageStarted: (String url) {
-          print('Page started loading: $url');
-        },
-        onPageFinished: (String url) {
-          print('Page finished loading: $url');
-        },
-        gestureNavigationEnabled: true,
-      ),
-      Center(
-          child: Chewie(
-        controller: _chewieController,
-      ))
+    _tabs = [
+      Loading(
+          position: position,
+          child: Tweets(tweets: _tweets, start: start, end: end)),
+      Loading(
+          position: position,
+          child: WebView(
+              initialUrl: 'https://www.qe.com.qa/wp/mws/tabs/tab1',
+              javascriptMode: JavascriptMode.unrestricted,
+              navigationDelegate: (NavigationRequest request) {
+                if (request.url.startsWith('https://www.youtube.com/') ||
+                    request.url.startsWith('https://www.google.com/')) {
+                  return NavigationDecision.prevent;
+                }
+                return NavigationDecision.navigate;
+              },
+              onPageStarted: startLoading,
+              onPageFinished: doneLoading,
+              gestureNavigationEnabled: true)),
+      Loading(
+          position: position,
+          child: Center(
+              child: AspectRatio(
+                  aspectRatio: 3 / 2,
+                  child: WebView(
+                      initialUrl: Uri.dataFromString(
+                              '<html><body><iframe style="width:100%;height:100%;top:0;left:0;position:absolute;" src="https://www.youtube.com/embed/taAYP6uAGvs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></body></html>',
+                              mimeType: 'text/html')
+                          .toString(),
+                      onPageStarted: startLoading,
+                      onPageFinished: doneLoading,
+                      javascriptMode: JavascriptMode.unrestricted))))
     ];
 
     return Scaffold(
         key: _scaffoldKey,
         extendBodyBehindAppBar: false,
-        backgroundColor: Color(0xFFE5E5E5),
+        backgroundColor: _currentIndex == 1 ? Colors.white : Color(0xFFE5E5E5),
         drawer: CustomDrawer(),
         appBar: PreferredSize(
             preferredSize:
                 Size(double.infinity, ConsDimensions.LargeAppBarHeight.h),
             child: DefaultAppBar(
+                bgColor: _currentIndex == 1 ? Colors.white : null,
                 allowHorizontalPadding: true,
                 title: _screenTitle,
                 bgImage: Localizations.localeOf(context).languageCode == 'en'
@@ -164,10 +238,11 @@ class _HomeState extends State<Home> {
                     height: 27.h,
                     width: 64.w,
                     color: Color(0xFFE1A854),
-                    onButtonPressed: () => print("pressed")),
-                onLeadingClicked: () =>
-                    _scaffoldKey.currentState.openDrawer())),
-        body: _children[_currentIndex],
+                    onButtonPressed: () => showAlertDialog(context)),
+                onLeadingClicked: () {
+                  _scaffoldKey.currentState.openDrawer();
+                })),
+        body: _tabs[_currentIndex],
         bottomNavigationBar: Theme(
             data: Theme.of(context).copyWith(canvasColor: Color(0xFF86C2C2)),
             child: _showBottomNavBar
@@ -179,21 +254,25 @@ class _HomeState extends State<Home> {
                         AppFonts.title9(color: Color(0XFF426676)),
                     onTap: (int index) {
                       setState(() {
+                        position = 1;
                         _currentIndex = index;
+                        switch (_currentIndex) {
+                          case 0:
+                            getTwitterNews();
+                            _screenTitle = translate("news");
+                            _showBottomNavBar = true;
+                            break;
+                          case 1:
+                            _screenTitle = translate("stock_exchange");
+                            _showBottomNavBar = false;
 
-                        _currentIndex == 2
-                            ? _chewieController.play()
-                            : _chewieController.pause();
-                        _currentIndex == 1
-                            ? _showBottomNavBar = false
-                            : _showBottomNavBar = true;
+                            break;
+                          case 2:
+                            _screenTitle = translate("live");
+                            _showBottomNavBar = true;
 
-                        if (_currentIndex == 0)
-                          _screenTitle = translate("news");
-                        else if (_currentIndex == 1)
-                          _screenTitle = translate("stock_exchange");
-                        else if (_currentIndex == 2)
-                          _screenTitle = translate("qatar_live");
+                            break;
+                        }
                       });
                     },
                     showSelectedLabels: true,
@@ -212,6 +291,21 @@ class _HomeState extends State<Home> {
                             title: Text(translate("live")))
                       ])
                 : Container(height: 0)));
+  }
+}
+
+class Loading extends StatelessWidget {
+  const Loading({Key key, @required this.position, @required this.child})
+      : super(key: key);
+
+  final int position;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return IndexedStack(
+        index: position,
+        children: [child, Center(child: CircularProgressIndicator())]);
   }
 }
 

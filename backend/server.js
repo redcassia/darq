7ec48d03b58b225e061c5368ffbda74a57216e4f
Noah Orensa <noahorensa@gmail.com>
2020-10-27 23:16:25 -0600

    optional startup of redirect server

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 var express = require('express')
 var app = require('./api')
 
-if (process.env.CERT_FILE) {
+if (process.env.CERT_FILE && process.env.KEY_FILE && process.env.HTTPS_PORT) {
   var key = fs.readFileSync(process.env.KEY_FILE);
   var cert = fs.readFileSync(process.env.CERT_FILE);
 
@@ -16,30 +16,37 @@ if (process.env.CERT_FILE) {
   https_server.listen(
     process.env.HTTPS_PORT, 
     () => {
-      console.log('HTTPS: WebUI server started on localhost:' + process.env.HTTPS_PORT);
-      console.log('HTTPS: Attachments server started on localhost:' + process.env.HTTPS_PORT + '/attachment');
-      console.log('HTTPS: GraphQL API server started on localhost:' + process.env.HTTPS_PORT + '/api');
+      console.log(`HTTPS: WebUI server listening on ${process.env.DOMAIN}:${process.env.HTTPS_PORT}`);
+      console.log(`HTTPS: Attachments server listening on ${process.env.DOMAIN}:${process.env.HTTPS_PORT}/attachment`);
+      console.log(`HTTPS: GraphQL API server listening on ${process.env.DOMAIN}:${process.env.HTTPS_PORT}/api`);
     }
   );
 }
 else {
-  console.log('WARNING: no SSL certificate found! Cannot start HTTPS server.')
+  console.log('WARNING: Unable to start HTTPS server.')
 }
 
-app.listen(
-  process.env.HTTP_PORT, 
-  () => {
-    console.log('HTTP: WebUI server started on localhost:' + process.env.HTTP_PORT);
-    console.log('HTTP: Attachments server started on localhost:' + process.env.HTTP_PORT + '/attachment');
-    console.log('HTTP: GraphQL API server started on localhost:' + process.env.HTTP_PORT + '/api');
-  }
-);
+if (process.env.HTTP_PORT) {
+  app.listen(
+    process.env.HTTP_PORT, 
+    () => {
+      console.log(`HTTP: WebUI server listening on ${process.env.DOMAIN}:${process.env.HTTP_PORT}`);
+      console.log(`HTTP: Attachments server listening on ${process.env.DOMAIN}:${process.env.HTTP_PORT}/attachment`);
+      console.log(`HTTP: GraphQL API server listening on ${process.env.DOMAIN}:${process.env.HTTP_PORT}/api`);
+    }
+  );
+}
+else {
+  console.log('WARNING: Unable to start HTTP server.')
+}
 
-var http_to_https = express();
+if (process.env.HTTPS_REDIRECT_DOMAIN && process.env.HTTP_TO_HTTPS_PORT) {
+  var http_to_https = express();
 
-// set up a route to redirect http to https
-http_to_https.get('*', function(req, res) {  
-  res.redirect('https://' + process.env.HTTPS_REDIRECT_DOMAIN + req.url);
-});
+  // set up a route to redirect http to https
+  http_to_https.get('*', function(req, res) {  
+    res.redirect('https://' + process.env.HTTPS_REDIRECT_DOMAIN + req.url);
+  });
 
-http_to_https.listen(process.env.HTTP_TO_HTTPS_PORT);
+  http_to_https.listen(process.env.HTTP_TO_HTTPS_PORT);
+}

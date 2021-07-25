68ff3ac41462d583d1f427ca3aff5ba44f654326
Noah Orensa <noahorensa@gmail.com>
2021-07-25 10:44:10 -0600

    overload Database.query() to support options object & add methods to promisify transactions

--- backend/api/database.js
+++ backend/api/database.js
@@ -7,30 +7,80 @@ class Database {
   }
 
   query(sql, args) {
+    var options;
+
+    if (typeof sql === "object") {
+      if (args !== undefined) {
+        throw new TypeError("Args cannot be defined when using an options object");
+      }
+
+      options = sql;
+    }
+    else {
+      options = {
+        sql: sql,
+        args: args,
+        release: true
+      };
+    }
+
+    return new Promise(async (resolve, reject) => {
+      try {
+        if (! options.conn) {
+          options.conn = await this.getConnection();
+        }
+
+        options.conn.query(options.sql, options.args, (err, rows) => {
+          if (options.release) options.conn.release();
+
+          if (err) reject(err);
+          else resolve(rows);
+        });
+      }
+      catch (e) {
+        reject(e);
+      }
+    });
+  }
+
+  getConnection() {
     return new Promise((resolve, reject) => {
-      this.pool.getConnection(function(err, connection) {
+      this.pool.getConnection((err, connection) => {
         if (err) {
           console.error("Error getting a MySQL connection from pool. ", err);
           reject(err);
         }
         else {
-          connection.query(sql, args, (err, rows) => {
-            connection.release();
-  
-            if (err) {
-              reject(err);
-            }
-            else {
-              resolve(rows);
-            }
-          });
+          resolve(connection);
         }
       });
     });
   }
 
-  connection() {
-    return this.pool.getConnection;
+  beginTransaction(connection) {
+    return new Promise((resolve, reject) => {
+      connection.beginTransaction((err) => {
+        if (err) reject(err);
+        else resolve();
+      });
+    });
+  }
+
+  commit(connection) {
+    return new Promise((resolve, reject) => {
+      connection.commit((err) => {
+        if (err) reject(err);
+        else resolve();
+      });
+    });
+  }
+
+  rollback(connection) {
+    return new Promise((resolve, reject) => {
+      connection.rollback(() => {
+        resolve();
+      });
+    });
   }
 
   close() {
